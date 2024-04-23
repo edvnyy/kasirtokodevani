d7835ae833464794d029fdcea44215c4a0e9fccd
Devani Amelia Pratiwi <devaniamelia2018@gmail.com>
2024-04-23 13:53:33 +0700

    pelanggan controller

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -14,44 +14,27 @@ class PelangganController extends Controller
     public function index(Request $request)
     {
         $search = $request->search;
-        $pelanggans = Pelanggan::orderBy('id')
-            ->when($search, function ($q) use ($search) {
-                return $q->where('nama', 'like', "%{$search}%");
-            })
-            ->paginate();
+        $query = Pelanggan::orderBy('id');
 
         if ($search) {
-            $pelanggans->appends(['search' => $search]);
+            $query->where('nama', 'like', "%{$search}%");
         }
+
+        // Filter pelanggan hanya yang memiliki status "Member"
+        $pelanggans = $query->where('status_member', 'Member')->paginate();
 
         return view('pelanggan.index', compact('pelanggans'));
     }
+
 
     public function create()
     {
         return view('pelanggan.create');
     }
 
-    public function store(Request $request)
-    {
-        try {
-            $request->validate([
-                'nama' => ['required', 'max:100'],
-                'alamat' => ['nullable', 'max:500'],
-                'nomor_tlp' => ['nullable', 'max:14']
-            ]);
 
-            Pelanggan::create($request->all());
 
-            return redirect()->route('pelanggan.index')->with('store', 'success');
-        } catch (QueryException $e) {
-            if ($e->errorInfo[1] === 1406) { // Error code 1406: Data too long for column
-                return redirect()->back()->withInput()->with('error', 'Alamat terlalu panjang.');
-            }
-            // Re-throw the exception if it's not the expected one
-            throw $e;
-        }
-    }
+
 
     public function show(Pelanggan $pelanggan)
     {
